9650c9a2c0d6291355aa32e1c32bb272a778fabc
32d <105021055@live.asia.edu.tw>
2017-10-31 13:01:41 +0800

    1

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class MainFrame extends JFrame {
     LoginFrame login = new LoginFrame();
@@ -14,43 +15,89 @@ public class MainFrame extends JFrame {
     private JMenu Jloto = new JMenu("Lotto");
     private JMenuItem Jexit = new JMenuItem("Exit");
     private JMenuItem Jlotto = new JMenuItem("Lotto");
+    private Random rdm = new Random(System.currentTimeMillis());
     private JDesktopPane Jdp = new JDesktopPane();
     private JInternalFrame JIFloto = new JInternalFrame("lotto");
     private JPanel JPnum = new JPanel(new GridLayout(1,6,5,5));
     private JPanel JPre = new JPanel(new GridLayout(1,2,3,3));
     private JButton JBclose = new JButton("close");
     private JButton JBre = new JButton("Re");
+    private JLabel Jlab[] = new JLabel[6];
+    private int JLableNum[] = new int[6];
+    private  JInternalFrame Jinkey = new JInternalFrame("inkey");
+
 
 
     public MainFrame(LoginFrame Login){
         login = Login ;
         init();
     }
-    private void init(){
-    this.setBounds(scrW/2-w/2,scrH/2-h/2,w,h);
-    this.setJMenuBar(Jmb);
-    Jmb.add(Jmu);
-    Jmb.add(Jloto);
-    Jmu.add(Jexit);
-    Jloto.add(Jlotto);
-    JIFloto.setLayout(new BorderLayout(3,3));
-    JPnum.add(JBclose);
-    JPnum.add(JBre);
-    JIFloto.add(JPnum);
-    JIFloto.add(JPre);
+    private void init() {
+        this.setBounds(scrW / 2 - w / 2, scrH / 2 - h / 2, w, h);
+        this.setJMenuBar(Jmb);
+        this.setContentPane(Jdp);
+        JIFloto.setBounds(100,100,600,200);
+        Jmb.add(Jmu);
+        Jmb.add(Jloto);
+        Jmu.add(Jexit);
+        Jdp.add(JIFloto);
+        Jloto.add(Jlotto);
+        JIFloto.setLayout(new BorderLayout(3, 3));
+        JPre.add(JBclose);
+        JPre.add(JBre);
+        Jdp.add(Jinkey);
+        Jinkey.setLayout(new BorderLayout(3, 3));
+        JIFloto.add(JPnum, BorderLayout.CENTER);
+        JIFloto.add(JPre, BorderLayout.SOUTH);
+        regenerate();
 
-    Jexit.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.exit(0);
+        for(int i =0;i<6;i++) {
+         Jlab[i] = new JLabel();
+         JPnum.add(Jlab[i]);
+         Jlab[i].setText(Integer.toString(JLableNum[i]));
         }
-    });
 
-    Jexit.setAccelerator(KeyStroke.getKeyStroke('X',Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
 
+
+        Jexit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+        Jexit.setAccelerator(KeyStroke.getKeyStroke('X', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+
+        Jlotto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JIFloto.setVisible(true);
+            }
+        });
 
 
 
 
     }
+
+    private void regenerate() {
+        boolean check;
+        for (int i = 0; i < 6; i++) {
+            check = true;
+            while (check) {
+                check = false;
+                int tmp = rdm.nextInt(49) + 1;
+                for (int j = 0; j < i; j++) {
+                    if (JLableNum[j] == tmp) {
+                        check = true;
+                    }
+                }
+                JLableNum[i] = tmp;
+            }
+        }
+    }
+
+
 }
+
